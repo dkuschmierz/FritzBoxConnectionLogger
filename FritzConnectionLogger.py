07840a090bb69b36c87224ca7b97cfc9e7ffdf52
Dominic Kuschmierz <nic@Dominics-iMac-2.fritz.box>
2020-08-28 16:35:01 +0200

    Added connection error handling

--- FritzConnectionLogger.py
+++ FritzConnectionLogger.py
@@ -15,11 +15,8 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 def main(args):
-    signal.signal(signal.SIGINT, signal_handler)
-    
-    kwargs = dict(address=args.address, port=args.port, user=args.user, password=args.password, timeout=args.timeout, use_tls=args.encryption)
-    fc = FritzConnection(**{k: v for k, v in kwargs.items() if v is not None})
-    fs = FritzStatus(fc)
+    signal.signal(signal.SIGINT, signal_handler)  
+
     
     print("\nExit program by pressing Ctrl+C\n")
     if args.verbose:
@@ -30,35 +27,48 @@ def main(args):
         fieldNames = ['Timestamp','PhyLink','Sync','Cur. Upstream','Cur. Downstream','Max. Upstream','Max. Downstream','Line Capacity Up','Line Capacity Down','SNR Up','SNR Down','Att. Up','Att. Down']
         csvWriter = csv.DictWriter(csvFile,fieldnames=fieldNames)
         csvWriter.writeheader()
-
+        
+        fc = None
+        
         while True:
-            att_upstream, att_downstream = fs.str_attenuation
-            snr_upstream, snr_downstream = fs.str_noise_margin
-            curstream_up, curstream_down = fs.str_transmission_rate
-            maxstream_up, maxstream_down = fs.str_max_bit_rate
-            line_up, line_down = fs.str_max_linked_bit_rate
+            try:
+                if fc == None:
+                    kwargs = dict(address=args.address, port=args.port, user=args.user, password=args.password, timeout=args.timeout, use_tls=args.encryption)
+                    fc = FritzConnection(**{k: v for k, v in kwargs.items() if v is not None})
+                    fs = FritzStatus(fc)
+                
+                
+                att_upstream, att_downstream = fs.str_attenuation
+                snr_upstream, snr_downstream = fs.str_noise_margin
+                curstream_up, curstream_down = fs.str_transmission_rate
+                maxstream_up, maxstream_down = fs.str_max_bit_rate
+                line_up, line_down = fs.str_max_linked_bit_rate
+                
+                dataDict = {
+                    'Timestamp': datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
+                    'PhyLink' : fs.is_connected,
+                    'Sync' : fs.is_linked,
+                    'Cur. Upstream' : curstream_up,
+                    'Cur. Downstream' : curstream_down,
+                    'Max. Upstream' : maxstream_up,
+                    'Max. Downstream' : maxstream_down,
+                    'Line Capacity Up' : line_up,
+                    'Line Capacity Down' : line_down,
+                    'SNR Up' : snr_upstream,
+                    'SNR Down' : snr_downstream,
+                    'Att. Up' : att_upstream,
+                    'Att. Down' : att_downstream
+                }
+                
+                csvWriter.writerow(dataDict)
+                csvFile.flush()
+                
+                if args.verbose:               
+                    print(" {0:<19} | {1:<7} | {2:<4} | {3:<13} | {4:<15} | {5:<13} | {6:<15} |  {7:<15} | {8:<18} | {9:<7} | {10:<8} | {11:<7} | {12:<8}".format(datetime.now().strftime("%d.%m.%Y %H:%M:%S"),fs.is_connected, fs.is_linked, curstream_up, curstream_down, maxstream_up , maxstream_down, line_up, line_down, snr_upstream, snr_downstream, att_upstream, att_downstream))
+            except ConnectionRefusedError:
+                print("Could not connect to FritzBox. Try to establish new connection on next cycle")
+                fc = None
             
-            dataDict = {
-                'Timestamp': datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
-                'PhyLink' : fs.is_connected,
-                'Sync' : fs.is_linked,
-                'Cur. Upstream' : curstream_up,
-                'Cur. Downstream' : curstream_down,
-                'Max. Upstream' : maxstream_up,
-                'Max. Downstream' : maxstream_down,
-                'Line Capacity Up' : line_up,
-                'Line Capacity Down' : line_down,
-                'SNR Up' : snr_upstream,
-                'SNR Down' : snr_downstream,
-                'Att. Up' : att_upstream,
-                'Att. Down' : att_downstream
-            }
-            
-            csvWriter.writerow(dataDict)
-            csvFile.flush()
-            
-            if args.verbose:               
-                print(" {0:<19} | {1:<7} | {2:<4} | {3:<13} | {4:<15} | {5:<13} | {6:<15} |  {7:<15} | {8:<18} | {9:<7} | {10:<8} | {11:<7} | {12:<8}".format(datetime.now().strftime("%d.%m.%Y %H:%M:%S"),fs.is_connected, fs.is_linked, curstream_up, curstream_down, maxstream_up , maxstream_down, line_up, line_down, snr_upstream, snr_downstream, att_upstream, att_downstream))
             time.sleep(args.cycleTime)
     
 
